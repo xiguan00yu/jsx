c8c387da2545354e97c444c9c56bf9eeecb1bee8
linkerlam <linkerlam@httpeace.com>
2021-06-20 14:44:45 +0800

    update npm package

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 export default {
     input: 'index.js',
     output: [
-        { file: 'dist/jsx.js', format: 'cjs', sourcemap: true },
+        { file: 'dist/jsx.umd.js', format: 'umd', name: 'jsx', sourcemap: true },
+        { file: 'dist/jsx.js', format: 'esm', sourcemap: true },
     ],
 }
